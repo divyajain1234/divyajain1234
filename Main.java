88e7760cb60b85de90b63a6e73e847d4a87db515
divyajain1234 <143693572+divyajain1234@users.noreply.github.com>
2023-09-13 12:53:22 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main{
+public static void main(String args[]){
+boolean a=true;
+  System.out.println(a); 
+  byte b=12; 
+  System.out.println(b); 
+  short c=-89; 
+  System.out.println(c);
+  int d=6473;
+  System.out.println(d); 
+  long e=65475436; 
+  System.out.println(e); 
+  double f=67.746; 
+  System.out.println(f); 
+  float g=12.2f; 
+  System.out.println(g); 
+  char h='9'; 
+  System.out.println(h); 
+  char i=65; 
+  System.out.println(i);
+}
