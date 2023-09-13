1f18b133092ea91f2e9437fe1e80df3f921e6559
divyajain1234 <143693572+divyajain1234@users.noreply.github.com>
2023-09-13 14:08:47 +0530

    inputoutputuser.java

---
+++ inputoutputuser.java
@@ -0,0 +1,8 @@
+//18. Demonstrate the use of Scanner class for taking input/output from user:-
+import java.util.Scanner;
+public class A{
+public static void main(String args[]){
+System.out.println("Enter any number: "); Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+System.out.println(n);
+} }
