fc25d4fbd69151bdc73d5a39bf89f8f15bd096fa
divyajain1234 <143693572+divyajain1234@users.noreply.github.com>
2023-09-13 14:04:11 +0530

    alphabetloop.java

---
+++ alphabetloop.java
@@ -0,0 +1,7 @@
+// Display uppercased using for
+import java.util.Scanner;
+public class Main{
+public static void main(String args[]){
+alphabet loop:-
+ System.out.println("The uppercased alphabets are:"); for(char i='A';i<='Z';i++){
+System.out.println(i); }
