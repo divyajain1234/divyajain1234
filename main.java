df033115cf01187584fba176cbb190bc302bdeb7
divyajain1234 <143693572+divyajain1234@users.noreply.github.com>
2023-09-13 14:13:40 +0530

    wantsum.java

---
+++ main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner:
+public class Main
+{
+public static void main(String arg[])
+{
+int n, sum-0;
+Scanner sc=new Scanner (System.in);
+System.out printIn("enter number till you want sum"); n-sc.nextInt);
+int a[]-new int[n];
+System.out.printIn("enter the "+n+" numbers ")
+for (int i-0;i‹n;i++)
+{
+System.out.printIn("enter number "+(i+1)+":");
+a[i]-sc.nextInt ();
+}
+{
+for(int 1=0;i<n;i+*)
+sum+=a[i];
+}
+System.out.printin("sum of "+n+* numbers is ="+sum);
+}
+}
