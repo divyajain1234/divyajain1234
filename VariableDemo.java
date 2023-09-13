e2c8b6ae5cca113414c033a0daaa0b374387a449
divyajain1234 <143693572+divyajain1234@users.noreply.github.com>
2023-09-13 14:17:33 +0530

    VariableDemo.java

---
+++ VariableDemo.java
@@ -0,0 +1,16 @@
+class VariableDemo
+static int count=0;
+public void increment)
+{
+count++;
+  }
+public static void main(String args[])
+{
+VariableDemo obj1-new VariableDemo();
+VariableDemo obj2-new VariableDemo ();
+obj1. increment ();
+obj2. increment ();
+System.out.printin("Obj1: count is-"+obj1.count);
+System.out.printin("Obj2: count is-"+obj2. count);
+}
+}
