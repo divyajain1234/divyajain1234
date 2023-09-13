f6f7e48f31aa3e2bfbcff0a9d658296e31904f8e
divyajain1234 <143693572+divyajain1234@users.noreply.github.com>
2023-09-13 14:00:18 +0530

    positive.java

---
+++ positive.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+public static void main(String args[]){
+  
+System.out.println("Enter any number: "); Scanner ob1=new Scanner(System.in); int a=ob1.nextInt();
+if(a>0){
+System.out.println("The number is positive."); }else if(a<0){
+System.out.println("The number is negative."); }
+else{ System.out.println("Zero");
+} }
+}
