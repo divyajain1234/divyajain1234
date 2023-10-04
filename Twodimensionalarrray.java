a6e550a145001a9e9b16c73b74dc78eb2490396c
divyajain1234 <143693572+divyajain1234@users.noreply.github.com>
2023-10-04 12:39:32 +0530

    Twodimensionalarrray.java

---
+++ Twodimensionalarrray.java
@@ -0,0 +1,11 @@
+class TwoDimensionalArray
+{
+    public static void main(String[] args)
+    {
+        String[][] salutation = {{"Mr.","Mrs.","Ms."},
+        {"Kumar"}};
+       System.out.println(salutation[0][0]+salutation[1][0]);
+       System.out.println(salutation[0][1]+salutation[1][0]);
+        
+    }
+}
